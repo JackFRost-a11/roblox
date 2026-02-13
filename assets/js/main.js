8ab9be76f375d8dbdf966fb840eaf0cc6c24e168
JackFRost-a11 <jackjrall@gmail.com>
2026-02-13 02:26:33 +0000

    BLOX

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
 // main.js
 
 console.log("Hello, World!");
+
+let targetElement = document.querySelector("h1");
+
+console.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function () {
+	console.log(this.innerText);
+	this.innerText = "Pay Now!";
+	this.style = "cursor: wait";
+
+	targetElement.addEventListener("mouseout", function () {
+	this.innerText = "Play Now!";
+	this.style = "cursor: pointer";
+	});
+});
